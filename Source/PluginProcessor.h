c698ccdc878fe0911ac2173e144a4c020aec53ae
Vishist Bhagabati <vishist.bhagabati123@gmail.com>
2024-08-21 01:34:26 +0530

    plugin generic UI and parameters made

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,6 +53,14 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    //this thingie made static coz no member variables
+    static juce::AudioProcessorValueTreeState::ParameterLayout
+      createParameterLayout();
+
+    juce::AudioProcessorValueTreeState apvts{
+      *this,nullptr,"Parameters",createParameterLayout()
+    };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
