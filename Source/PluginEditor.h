338a02739ee36b5f1031f9cf7c6ee640df656fe8
Vishist Bhagabati <vishist.bhagabati123@gmail.com>
2024-10-15 18:07:04 +0530

    sliders and parameters connected

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -45,6 +45,19 @@ private:
     lowCutSlopeSlider,
     highCutSlopeSlider;
 
+
+    // aliases as names very long
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+
+    Attachment peakFreqSliderAttachment,
+    peakGainSliderAttachment,
+    peakQualitySliderAttachment,
+    lowCutFreqSliderAttachment,
+    highCutFreqSliderAttachment,
+    lowCutSlopeSliderAttachment,
+    highCutSlopeSliderAttachment;
+
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
